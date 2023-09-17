5d203d2eee9b4a92d33113f723903517e3fd6f3d
danbuitrago87 <danbuitrago87@gmail.com>
2023-09-17 17:47:02 -0500

    cambios build

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 # exit on error
 set -o errexit
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python world_champ_2022/manage.py createsuperuser --no-input
+fi
+
 #poetry install
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
