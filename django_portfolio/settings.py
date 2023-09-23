ed9d443acf90dd46175e2c924b81215a6a3708a5
danbuitrago87 <danbuitrago87@gmail.com>
2023-09-23 11:25:09 -0500

    otraBD

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -81,15 +81,18 @@ WSGI_APPLICATION = "django_portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    #"default": {
-        #"ENGINE": "django.db.backends.sqlite3",
-        #"NAME": BASE_DIR / "db.sqlite3",
-        'default': dj_database_url.config(        
-            default='postgresql://postgres:postgres@localhost:5432/mysite',       
-            conn_max_age=600)
-    }
-
+#DATABASES = {
+ #   "default": {
+  #      "ENGINE": "django.db.backends.sqlite3",
+   #     "NAME": BASE_DIR / "db.sqlite3",
+    #'default': dj_database_url.config(        
+     #   default='postgresql://postgres:postgres@localhost:5432/mysite',       
+      #  conn_max_age=600)
+#}
+#}
+DATABASES={
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
